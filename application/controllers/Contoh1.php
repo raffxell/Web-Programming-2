ae6686522496246232ef2010b658b9feda2366de
raffxell <raffieelbasyar29@gmail.com>
2024-03-26 19:53:05 +0700

    menambahkan index.html, contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Raffie El Basyar Saya Tinggal Di daerah bekasi olah raga yang saya sukai adalah Bulutangkis";
+ }
+}
